8efe8c22c7727dbb6b2bfe674f59175d66e6fa63
Lord Javac <lordjavac@gmail.com>
2015-07-27 15:22:27 -0700

    Moved tests under main package.

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup, find_packages
 setup(
     name       = '6-Nimt',
     version    = '0.1',
-    packages   = find_packages(),
-    test_suite = 'unittest.main'
+    packages   = ['nimt6', 'nimt6.tests'],
+    classifiers = [
+        'Development Status :: 1 - Planning',
+        'Topic :: Games'
+    ],
+    test_suite = 'nimt6.tests'
 )
